baf5adde70ca3a2ec85e13ea45863d1f1186bac4
JDommett@gmail.com <JDommett@gmail.com@3e0f000e-2220-dfdd-a6fd-f41013e3e758>
2014-04-05 23:53:16 +0000

    [Jenny] organise gather.sh into functions

--- scripts/gather.sh
+++ scripts/gather.sh
@@ -19,7 +19,7 @@ function sourceProperties()
 {
     propertyFilePath=$1
 
-    if [ ! -f $propertyFilePath ]; then
+    if [ ! -f ${propertyFilePath} ]; then
         echo "Cannot find $propertyFilePath"
         exit 1
     fi
@@ -34,58 +34,73 @@ function sourceProperties()
     echo ""
 }
 
+function clean()
+{
+    rm -rf $1
+    mkdir $1
+}
+
+function compileSources()
+{
+    find ${sources} -name "*.java" | xargs javac -d $1 -cp "$classpath"
+    echo $(result)
+}
+
+function compileTests()
+{
+    find ${tests} -name "*Test.java" | xargs javac -d $1 -cp "$1:$classpath"
+    echo $(result)
+}
+
+function runTests()
+{
+    find $1 -name "*Test.class" | sed -e "s/.class//; s/$1\///; s/\//./g;" | xargs java -cp "$1:$classpath" org.junit.runner.JUnitCore >/dev/null 2>&1
+    echo $(result)
+}
+
+function saveDataToCSV()
+{
+    dataFolder="data"
+    dataFile="$dataFolder/$projectName.csv"
+
+    #create data folder if it doesn't exist
+    if [ ! -d ${dataFolder} ]; then
+        mkdir ${dataFolder}
+    fi
+
+    #create data file with header if it doesn't exist
+    if [ ! -f $dataFile ]; then
+        echo "#CSV format: sourceCompile, testCompile, testRun" > ${dataFile}
+    fi
+
+    #append data to project data file
+    echo "$1, $2, $3" >> ${dataFile}
+}
+
+function main()
+{
+    buildDir="build"
+
+    #start in miru root folder
+    cd ..
+
+    clean ${buildDir}
+    sourceProperties $1
+    sourceCompile=$(compileSources ${buildDir})
+    testCompile=$(compileTests ${buildDir})
+    testRun=$(runTests ${buildDir})
+
+    saveDataToCSV ${sourceCompile} ${testCompile} ${testRun}
+
+    echo "Results"
+    echo "--------"
+    echo "source compile: $sourceCompile"
+    echo "test compile:   $testCompile"
+    echo "tests pass:     $testRun"
+}
 
 if [ "$#" -ne 1 ]; then
     usage
 fi
 
-sourceProperties $1
-
-
-#location config
-buildDir="build"
-dataFolder="data"
-dataFile="$dataFolder/$projectName.csv"
-
-sourceCompile=0
-testCompile=0
-testRun=0
-
-#start in miru root folder
-cd ..
-
-#clean buildDir
-rm -rf $buildDir
-mkdir $buildDir
-
-echo "compiling sources..."
-find $sources -name "*.java" | xargs javac -d $buildDir -cp "$classpath"
-sourceCompile=$(result)
-
-echo "compiling tests..."
-find $tests -name "*Test.java" | xargs javac -d $buildDir -cp "$buildDir:$classpath"
-testCompile=$(result)
-
-echo "running tests..."
-find $buildDir -name "*Test.class" | sed -e "s/.class//; s/$buildDir\///; s/\//./g;" | xargs java -cp "$buildDir:$classpath" org.junit.runner.JUnitCore >/dev/null 2>&1
-testRun=$(result)
-
-echo ""
-echo "Results"
-echo "--------"
-echo "source compile: $sourceCompile"
-echo "test compile:   $testCompile"
-echo "tests pass:     $testRun"
-
-#create data folder if it doesn't exist
-if [ ! -d $dataFolder ]; then
-    mkdir $dataFolder
-fi
-
-#create data file with header if it doesn't exist
-if [ ! -f $dataFile ]; then
-    echo "#CSV format: sourceCompile, testCompile, testRun" > ${dataFile}
-fi
-
-#append data to project data file
-echo "$sourceCompile, $testCompile, $testRun" >> ${dataFile}
+main $1
