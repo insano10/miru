b8c3efcc59a8e4cf1d5e579f5581c99e0f6733ff
JDommett@gmail.com <JDommett@gmail.com@3e0f000e-2220-dfdd-a6fd-f41013e3e758>
2014-04-05 21:13:13 +0000

    [Jenny] write stats to csv file and shuffle around build/data directories

--- scripts/gather.sh
+++ scripts/gather.sh
@@ -2,24 +2,31 @@
 
 function result() {
    if [ $? -eq 0 ]; then
-      echo "OK"
+      echo "1"
    else
-      echo "FAIL"
+      echo "0"
    fi
 }
 
 #location config
+projectName="romanNumerals"
 root="/home/jenny/IdeaProjects/RomanNumerals"
 sources="${root}/src/main/java"
 tests="${root}/src/test/java"
 classpath="${root}/lib/*"
-buildDir="bin"
+buildDir="build"
 mainClass="com/lmax/dojo/romanNumerals/RomanNumeralConverter"
+dataFolder="data"
+dataFile="${dataFolder}/${projectName}"
 
 sourceCompile=0
 testCompile=0
 testRun=0
 
+#start in miru root folder
+cd ..
+
+#clean buildDir
 rm -rf ${buildDir}
 mkdir ${buildDir}
 
@@ -32,7 +39,7 @@ find ${tests} -name "*Test.java" | xargs javac -d ${buildDir} -cp "${buildDir}:$
 testCompile=$(result)
 
 echo "running tests..."
-find . -name "*Test.class" | sed -e "s/.class//; s/.\/${buildDir}\///; s/\//./g;" | xargs java -cp "${buildDir}:${classpath}" org.junit.runner.JUnitCore >/dev/null 2>&1
+find ${buildDir} -name "*Test.class" | sed -e "s/.class//; s/${buildDir}\///; s/\//./g;" | xargs java -cp "${buildDir}:${classpath}" org.junit.runner.JUnitCore >/dev/null 2>&1
 testRun=$(result)
 
 echo ""
@@ -41,3 +48,16 @@ echo "--------"
 echo "source compile: ${sourceCompile}"
 echo "test compile:   ${testCompile}"
 echo "tests pass:     ${testRun}"
+
+#create data folder if it doesn't exist
+if [ ! -d ${dataFolder} ]; then
+    mkdir ${dataFolder}
+fi
+
+#create data file with header if it doesn't exist
+if [ ! -f ${dataFile} ]; then
+    echo "#CSV format: sourceCompile, testCompile, testRun" > ${dataFile}
+fi
+
+#append data to project data file
+echo "${sourceCompile}, ${testCompile}, ${testRun}" >> ${dataFile}
