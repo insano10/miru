760471720e870d2ed72e2fad44bcb7f8d362408e
JDommett@gmail.com <JDommett@gmail.com@3e0f000e-2220-dfdd-a6fd-f41013e3e758>
2014-04-09 19:17:19 +0000

    [Jenny] finish migrating from chart.js to flot

--- src/main/webapp/js/miruCharts.js
+++ src/main/webapp/js/miruCharts.js
@@ -1,68 +1,46 @@
 //GLOBAL VARS
-var lineCountChart;
-var options = { animation : false };
+var options = {
+    series:
+    {
+        lines: { show: true, fill: 0.5 },
+        points: { show: false }
+    },
+    xaxis:
+    {
+        tickLength: 0,
+        mode: "time",
+        timeformat: "%H:%M:%S",
+        minTickSize: [5, "minute"]
+    },
+    grid:
+    {
+        borderColor: "rgba(220,220,220,1)"
+    },
+    yaxis:
+    {
+        tickLength: 0,
+        minTickSize: 1,
+        tickDecimals: 0,
+        tickFormatter: function(val, axis) { return val < axis.max ? val.toFixed(0) : "Line count";}
+    }
+};
 
 function initialiseLineCountChart()
 {
-//    var emptyData = createLineCountChartDataStructure([0], [0])
-//
-//    var canvas = document.getElementById("lineCountChart");
-//    var ctx = canvas.getContext("2d");
-//    lineCountChart = new Chart(ctx);
-//    lineCountChart.Line(emptyData, options);
-    var sourceSeries = { label: "source lines", data : [], shadowSize: 0};
-    var testSeries = { label: "test lines", data : [], shadowSize: 0};
+    var sourceSeries = { label: "source lines", data : [], color: "rgba(151,187,205,1)"};
+    var testSeries = { label: "test lines", data : [], color: "rgba(220,220,220,1)"};
     var data = [sourceSeries, testSeries];
-    var options = {
-        series: {
-            lines: { show: true },
-            points: { show: false }
-        }
-    };
-
     $("#lineCountChart").plot(data, options);
 }
 
 function updateLineCountChart(sourceDataPointArray, testDataPointArray)
 {
-//    var data = createLineCountChartDataStructure(sourceDataPointArray, testDataPointArray);
-//    lineCountChart.Line(data, options);
-
-    var sourceSeries = { label: "source lines", data : [[1,1],[2,2],[3,3]], shadowSize: 0};
-    var testSeries = { label: "test lines", data : [[1,3],[2,2],[3,1]], shadowSize: 0};
+    var sourceSeries = { label: "source lines", data : [[1397069754000,1],[1397069755000,2],[1397069756000,3]], shadowSize: 0, color: "rgba(151,187,205,1)"};
+    var testSeries = { label: "test lines", data : [[1397069754000,3],[1397069755000,2],[1397069756000,1]], shadowSize: 0, color: "rgba(220,220,220,1)"};
     var data = [sourceSeries, testSeries];
-    var options = {
-        series: {
-            lines: { show: true },
-            points: { show: false }
-        }
-    };
+
 
     $("#lineCountChart").plot(data, options);
-}
-
-function createLineCountChartDataStructure(sourceCountDataPoints, testCountDataPoints)
-{
-    var data = {
-        labels : ["","","","","","",""], //currently required :(
-        datasets : [
-            {
-                fillColor : "rgba(220,220,220,0.5)",
-                strokeColor : "rgba(220,220,220,1)",
-                pointColor : "rgba(220,220,220,1)",
-                pointStrokeColor : "#fff",
-                data : sourceCountDataPoints
-            },
-            {
-                fillColor : "rgba(151,187,205,0.5)",
-                strokeColor : "rgba(151,187,205,1)",
-                pointColor : "rgba(151,187,205,1)",
-                pointStrokeColor : "#fff",
-                data : testCountDataPoints
-            }
-        ]
-    };
-    return data;
 }
 
 $(document).ready(function(){
